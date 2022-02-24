54a4f1b805c0779c68cbff499dd6055220eff595
DivyaL2 <96220816+DivyaL2@users.noreply.github.com>
2022-02-24 16:43:06 +0000

    done the code

--- p2final.c
+++ p2final.c
@@ -19,7 +19,7 @@ int input_triangle(float x1, float y1, float x2, float y2, float x3,float y3)
 void output(float x1, float y1,float x2, float y2, float x3,float y3,int is_triangle)
 {
   if(is_triangle==1)
-    printf("the points (%f,%f),(%f,%f)and (%f,%f) forms 
+    printf("the points (%f,%f),(%f,%f)and (%f,%f) form 
     a  triangle",x1,y1,x2,y2,x3,y3);
   else
     printf("the points (%f,%f),(%f,%f)and (%f,%f) do not 
