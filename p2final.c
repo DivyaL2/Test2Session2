db7196b3e5109e1ae2919b81bb88dcce2c047dac
DivyaL2 <96220816+DivyaL2@users.noreply.github.com>
2022-02-24 16:07:12 +0000

    done the debug

--- p2final.c
+++ p2final.c
@@ -1,1 +1,36 @@
-
+#include <stdio.h>
+#include <math.h>
+void input(float *x1, float *y1, float *x2, float *y2,float *x3,float *y3)
+{
+  printf("enter the values of x and y\n");
+  scanf("%f%f%f%f%f%f",x1,y1,x2,y2,x3,y3);
+}
+int input_triangle(float x1, float y1, float x2, float y2, float x3,float y3)
+{
+  int a,b,c;
+  a=sqrt(pow((x2-x1),2)+pow((y2-y2),2));
+  b=sqrt(pow((x2-x1),2)+pow((y2-y2),2));
+  c=sqrt(pow((x2-x1),2)+pow((y2-y2),2));
+  if((a+b)>c && (b+c)>a && (c+a)>b)
+    return 1;
+  else
+    return 0;
+}    
+void output(float x1, float y1,float x2, float y2, float x3,float y3,int is_triangle)
+{
+  if(is_triangle==1)
+    printf("the points (%f,%f),(%f,%f)and (%f,%f) forms 
+    a  triangle\n",x1,y1,x2,y2,x3,y3);
+  else
+    printf("The points (%f,%f),(%f,%f)and (%f,%f) do not 
+    form a triangle\n",x1,y1,x2,y2,x3,y3);
+    
+}
+int main()
+{
+  float x1,y1,x2,y2,x3,y3,;
+  input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
+  int t=is_triangle(x1,y1,x2,y2,x3,y3);
+  output(x1,y1,x2,y2,x3,y3,t);
+  return 0;
+}
