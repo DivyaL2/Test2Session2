5c45163cbfb8946efd8bea1bff40a97857847bb4
DivyaL2 <96220816+DivyaL2@users.noreply.github.com>
2022-03-16 04:07:44 +0000

    done the program of p5final

--- p5final.c
+++ p5final.c
@@ -1,1 +1,31 @@
-
+#include<stdio.h>
+#include<math.h>
+int input_array_size()
+{
+  int n;
+  printf("enter the array size\n");
+  scanf("%d",&n);
+  return n;
+}
+void init_array(int n, int a[n])
+{
+  for(int i=0;i<n;i++)
+    {
+      a[i]=i+1;
+    }
+  }
+void erotosthenes_sieve(int n, int a[n])
+{
+  for(int i=1;i<n;i++)
+    {
+      if(a[i]==0)
+        continue;
+      for(j=1+1;j<=n;j++)
+        {
+          if(j%a[i]==0)
+            a[j]=0;
+          } 
+        }
+          
+    }
+}
